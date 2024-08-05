824c588242f970e3c520724ef520cbc83256252d
Stephboss9 <mbengastephane34@gmail.com>
2024-08-05 02:29:59 -0400

    updated api link to use hosted heroku backend

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -19,41 +19,42 @@ import ShoppingCart from "../ShoppingCart/ShoppingCart"
 import axios from "axios"
 
 export default function App() {
- const [products, setProducts] = useState([])
- const [isFetching, setFetching] = useState(false)
- const [error, setError] = useState(null)
- const [isSubmitted, setSubmitted] = useState(null)
- const [isOpen, setOpen] = useState(false) //represents the open/closed state of the sidebar
- const [shoppingCart, setShoppingCart] = useState([])
- const [input, setInput] = useState("")
- const [checkoutForm, setCheckoutForm] = useState({name:"", email:""})
- const [receipt, setReceipt] = useState(null)
- const [ordersError, setOrdersError] = useState(null)
- const [pastOrders, setPastOrders] = useState([])
+  const [products, setProducts] = useState([])
+  const [isFetching, setFetching] = useState(false)
+  const [error, setError] = useState(null)
+  const [isSubmitted, setSubmitted] = useState(null)
+  const [isOpen, setOpen] = useState(false) //represents the open/closed state of the sidebar
+  const [shoppingCart, setShoppingCart] = useState([])
+  const [input, setInput] = useState("")
+  const [checkoutForm, setCheckoutForm] = useState({ name: "", email: "" })
+  const [receipt, setReceipt] = useState(null)
+  const [ordersError, setOrdersError] = useState(null)
+  const [pastOrders, setPastOrders] = useState([])
 
   useEffect(() => {
-    axios.get('http://localhost:3001/store').then(response => {
+    axios.get('https://student-store-app-108b2e4e9abd.herokuapp.com/store').then(response => {
       setProducts(response.data.products)
       setFetching(true)
-    }).catch (error => {
+    }).catch(error => {
       setError("There was an error getting the data")
       setFetching(false)
-  });}
- , [])
-/*
- let getQuantity = (product) => {
-  let productData = shoppingCart.filter(current => {
-    (current.id === product.id)
-   })
-   console.log("getQuantity call check", productData)
-   return productData[0]
-}
-*/
- //Toggles the sidebar
- let handleOnToggle = ()=> {
+    });
+  }
+    , [])
+  /*
+   let getQuantity = (product) => {
+    let productData = shoppingCart.filter(current => {
+      (current.id === product.id)
+     })
+     console.log("getQuantity call check", productData)
+     return productData[0]
+  }
+  */
+  //Toggles the sidebar
+  let handleOnToggle = () => {
     let sidebar = document.querySelector(".sidebar");
     sidebar.classList.toggle("open")
-    if(isOpen){
+    if (isOpen) {
       setOpen(false)
       setSubmitted(null)
     }
@@ -62,96 +63,98 @@ export default function App() {
       setError(null)
       setReceipt(null)
     }
- }
+  }
 
- //adds items to the shopping cart
- let handleAddItemToCart = (productId) => {
-      let newProd = {itemId: productId, quantity:1}
-      let cartCopy = shoppingCart.filter(current => { if (current.itemId === productId){
-          newProd.quantity = current.quantity + 1;
-          return false;
-        } else {return true;}
-      }
-    )  
+  //adds items to the shopping cart
+  let handleAddItemToCart = (productId) => {
+    let newProd = { itemId: productId, quantity: 1 }
+    let cartCopy = shoppingCart.filter(current => {
+      if (current.itemId === productId) {
+        newProd.quantity = current.quantity + 1;
+        return false;
+      } else { return true; }
+    }
+    )
     cartCopy.push(newProd)
-      setShoppingCart(cartCopy)
-}
-//removes items from the shopping cart
-let handleRemoveItemFromCart  = (productId) => {
-  let newProd = {itemId: productId, quantity:1}
-      let cartCopy = shoppingCart.filter(current => { if (current.itemId === productId){
-          newProd.quantity = current.quantity - 1;
-          return false;
-        } else {return true;}
-      }
-    )  
-      if(newProd.quantity != 0){cartCopy.push(newProd)}
-      setShoppingCart(cartCopy)
-}
-//gets the user's info
-let handleOnCheckoutFormChange = (name, value) => {
- 
-  
-      setCheckoutForm(currentState =>  ({
-        ...currentState, [name]:value
-      }))
-}
-//submits the users order
-let handleOnSubmitCheckoutForm = async () => {
-  try {
-    let response =  await axios.post("http://localhost:3001/store" , {
-      shoppingCart,
-      user: {
-        name:checkoutForm.name, 
-        email:checkoutForm.email
-      }
+    setShoppingCart(cartCopy)
+  }
+  //removes items from the shopping cart
+  let handleRemoveItemFromCart = (productId) => {
+    let newProd = { itemId: productId, quantity: 1 }
+    let cartCopy = shoppingCart.filter(current => {
+      if (current.itemId === productId) {
+        newProd.quantity = current.quantity - 1;
+        return false;
+      } else { return true; }
+    }
+    )
+    if (newProd.quantity != 0) { cartCopy.push(newProd) }
+    setShoppingCart(cartCopy)
+  }
+  //gets the user's info
+  let handleOnCheckoutFormChange = (name, value) => {
+
+
+    setCheckoutForm(currentState => ({
+      ...currentState, [name]: value
+    }))
+  }
+  //submits the users order
+  let handleOnSubmitCheckoutForm = async () => {
+    try {
+      let response = await axios.post("https://student-store-app-108b2e4e9abd.herokuapp.com/store", {
+        shoppingCart,
+        user: {
+          name: checkoutForm.name,
+          email: checkoutForm.email
+        }
       })
       setReceipt(response.data.purchase.receipt)
       setError(null)
       console.log(response)
-  }
-  catch(error) {
-  
-    setError(error.response.data.error.message)
-  }
-}
-//gets the user's past orders
-let handleOnPastOrders = () => {
-  axios.get('http://localhost:3001/store/purchases').then(response => {
-    console.log(response)
-    setPastOrders(response.data.purchases)
-  }).catch (error => {
-    setOrdersError(error)
-})
-}
-//gets the quantity for each item the user has added
-let getQuantity = (currentProduct)=> {
-  let item = shoppingCart.find((current) => {
-    return (current.itemId === currentProduct.id)
-   })
-   return (!item?null:item.quantity)
+    }
+    catch (error) {
 
-}
+      setError(error.response.data.error.message)
+    }
+  }
+  //gets the user's past orders
+  let handleOnPastOrders = () => {
+    axios.get('https://student-store-app-108b2e4e9abd.herokuapp.com/store/purchases').then(response => {
+      console.log(response)
+      setPastOrders(response.data.purchases)
+    }).catch(error => {
+      setOrdersError(error)
+    })
+  }
+  //gets the quantity for each item the user has added
+  let getQuantity = (currentProduct) => {
+    let item = shoppingCart.find((current) => {
+      return (current.itemId === currentProduct.id)
+    })
+    return (!item ? null : item.quantity)
+
+  }
 
   return (
     <div className="app">
       <BrowserRouter>
         <main>
-          {}
+          { }
           <Navbar />
-          <Sidebar isOpen = {isOpen} products = {products} handleOnToggle = {handleOnToggle} shoppingCart = {shoppingCart}  setShoppingCart = {setShoppingCart}
-          setCheckoutForm = {setCheckoutForm}  isSubmitted = {isSubmitted} setSubmitted = {setSubmitted}
-          checkoutForm = {checkoutForm} handleOnCheckoutFormChange = {handleOnCheckoutFormChange} handleOnSubmitCheckoutForm = {handleOnSubmitCheckoutForm} error = {error}
-          receipt = {receipt} />
+          <Sidebar isOpen={isOpen} products={products} handleOnToggle={handleOnToggle} shoppingCart={shoppingCart} setShoppingCart={setShoppingCart}
+            setCheckoutForm={setCheckoutForm} isSubmitted={isSubmitted} setSubmitted={setSubmitted}
+            checkoutForm={checkoutForm} handleOnCheckoutFormChange={handleOnCheckoutFormChange} handleOnSubmitCheckoutForm={handleOnSubmitCheckoutForm} error={error}
+            receipt={receipt} />
           <Routes>
-              <Route path = "/" element ={<Home products = {products} input = {input} setInput = {setInput}  
-              handleRemoveItemToCart = {handleRemoveItemFromCart} handleAddItemToCart = {handleAddItemToCart} 
-              shoppingCart = {shoppingCart} getQuantity = {getQuantity}/>}/> 
-              <Route path= "/products/:productId" element={<ProductDetail getQuantity = {getQuantity}  handleRemoveItemToCart = {handleRemoveItemFromCart} handleAddItemToCart = {handleAddItemToCart}/>}/>
-              <Route path= "*" element ={<NotFound/>}/> 
-              <Route path = "/purchases" element = {<Orders handleOnPastPurchases = {handleOnPastOrders} orders = {ordersError} setOrdersError = {setOrdersError} setPastOrders = {setPastOrders} pastOrders = {pastOrders}/>}/>
+            <Route path="/" element={<Home products={products} input={input} setInput={setInput}
+              handleRemoveItemToCart={handleRemoveItemFromCart} handleAddItemToCart={handleAddItemToCart}
+              shoppingCart={shoppingCart} getQuantity={getQuantity} />} />
+            <Route path="/products/:productId" element={<ProductDetail getQuantity={getQuantity} handleRemoveItemToCart={handleRemoveItemFromCart} handleAddItemToCart={handleAddItemToCart} />} />
+            <Route path="*" element={<NotFound />} />
+            <Route path="/purchases" element={<Orders handleOnPastPurchases={handleOnPastOrders} orders={ordersError} setOrdersError={setOrdersError} setPastOrders={setPastOrders} pastOrders={pastOrders} />} />
           </Routes>
-          <Footer/>
+          <Footer />
         </main>
       </BrowserRouter>
     </div>
--- student-store-ui/src/components/ProductDetail/ProductDetail.jsx
+++ student-store-ui/src/components/ProductDetail/ProductDetail.jsx
@@ -9,32 +9,33 @@ import "./ProductDetail.css"
 
 
 export default function ProductDetail(props) {
-    const [product, setProduct] = useState({})
-    const [isLoading, setIsLoading] = useState(false)
-    let {productId} = useParams()
-    const[requestError, setRequestError] = useState(false)
-   
+  const [product, setProduct] = useState({})
+  const [isLoading, setIsLoading] = useState(false)
+  let { productId } = useParams()
+  const [requestError, setRequestError] = useState(false)
 
-    useEffect( () => {
-      try {
-        setIsLoading(true)
-        const obtainProduct = async () => {
-        const response = await (axios.get(`http://localhost:3001/store/${productId}`))
+
+  useEffect(() => {
+    try {
+      setIsLoading(true)
+      const obtainProduct = async () => {
+        const response = await (axios.get(`https://student-store-app-108b2e4e9abd.herokuapp.com/store/${productId}`))
         const responseData = response.data.product
-        setProduct(responseData)}
-        obtainProduct()
-        setIsLoading(false)
+        setProduct(responseData)
       }
-        catch(error){
-          return (<NotFound />)
-        }
-}, [])
+      obtainProduct()
+      setIsLoading(false)
+    }
+    catch (error) {
+      return (<NotFound />)
+    }
+  }, [])
 
   return (
     <div className='product-detail'>
-      {isLoading?<span className='id-container'><h1 className='loading'> Loading...</h1></span>:  <ProductView  product = {product} productId = {productId} quantity = {props.getQuantity(product)} handleAddItemToCart = {props.handleAddItemToCart}
-        handleRemoveItemToCart = {props.handleRemoveItemToCart}/>}
-       
+      {isLoading ? <span className='id-container'><h1 className='loading'> Loading...</h1></span> : <ProductView product={product} productId={productId} quantity={props.getQuantity(product)} handleAddItemToCart={props.handleAddItemToCart}
+        handleRemoveItemToCart={props.handleRemoveItemToCart} />}
+
     </div>
   )
 }
@@ -43,14 +44,14 @@ export default function ProductDetail(props) {
 export function ProductView(props) {
   return (
     <div className='product-view'>
-      
-        <span className='id-container'><h1 className='product-id'> Product #{props.productId}</h1></span>
-        <div className = "view-info">
-        <ProductCard product = {props.product} showDescription = {true} productId = {props.productId}
-        quantity = {props.quantity} handleAddItemToCart = {props.handleAddItemToCart}
-        handleRemoveItemToCart = {props.handleRemoveItemToCart}/>
-        </div>
-        
+
+      <span className='id-container'><h1 className='product-id'> Product #{props.productId}</h1></span>
+      <div className="view-info">
+        <ProductCard product={props.product} showDescription={true} productId={props.productId}
+          quantity={props.quantity} handleAddItemToCart={props.handleAddItemToCart}
+          handleRemoveItemToCart={props.handleRemoveItemToCart} />
+      </div>
+
     </div>
   )
 }
